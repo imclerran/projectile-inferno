a65877dec656a222450bda217d6d81507884a4c1
cnguyen <chris.m.nguyen@wsu.edu>
2019-04-10 15:19:38 -0700

    fix some minor typos on btf

--- core/src/com/droptableteams/game/util/types/BossTypeFactory.java
+++ core/src/com/droptableteams/game/util/types/BossTypeFactory.java
@@ -18,12 +18,12 @@ public class BossTypeFactory {
 
     public static BossType makeMidBoss() {
         FirePatternData fpd = new FirePatternData(Directions.DOWN, 7, (float)(3.0), (float)(Math.PI), 0.4f, "PlayerBullet");
-        return new BossType("EnemyEntity", "EnemyA",fpd, 52, 52, 128, 100, true, false,"sprites/bossA.png");
+        return new BossType("BossEntity", "midBoss",fpd, 52, 52, 128, 100, true, false,"sprites/bossA.png");
     }
 
     public static BossType makeFinalBoss() {
         FirePatternData fpd = new FirePatternData(Directions.DOWN, 6, (float)(Math.PI), (float)(Math.PI), 0.4f, "EnemyBulletA");
-        return new BossType("EnemyEntity", "EnemyB",fpd, 52, 52, 128, 100, true, false,"sprites/bossB.png");
+        return new BossType("BossEntity", "finalBoss",fpd, 52, 52, 128, 100, true, false,"sprites/bossB.png");
     }
 
 }
