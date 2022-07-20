19d8df756eb2a7e15f93c0eb7ee962dfc2e424fd
Ian McLerran <imclerran@protonmail.com>
2022-07-20 15:05:01 -0700

    Added TODOs + GH issues for SpecialEntityIds

--- core/src/com/droptableteams/game/util/constants/SpecialEntityIds.java
+++ core/src/com/droptableteams/game/util/constants/SpecialEntityIds.java
@@ -4,15 +4,17 @@ public class SpecialEntityIds {
 
     public static final int GAME_ENTITY = -1;
     public static final int PLAYER_ENTITY = -2;
-    public static final int VISIBLE_HITBOX_ENTITY = -3;
-    public static final int SHIELD_ENTITY = -4;
+    public static final int VISIBLE_HITBOX_ENTITY = -3; // TODO: consider using normally assigned id for hitbox as well
+    public static final int SHIELD_ENTITY = -4; // TODO: shield entity does not need special id
 
-    @Deprecated
+    // TODO: #6 Refactor codebase to remove deprecated SpecialEntityIds getter methods
+
+    @Deprecated(forRemoval = true)
     public static int getGameEntityId() { return GAME_ENTITY; }
 
-    @Deprecated
+    @Deprecated(forRemoval = true)
     public static int getPlayerEntityId() { return PLAYER_ENTITY; }
 
-    @Deprecated
+    @Deprecated(forRemoval = true)
     public static int getVisibleHitboxEntity() { return VISIBLE_HITBOX_ENTITY; }
 }
